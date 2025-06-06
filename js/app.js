ec65c103d47a0a49cc597d768ea67d9e87e3ab2e
Luis Gutierrez <lgutierrez@mstecs.com>
2025-06-06 13:23:38 -0600

    header and bubble buttons

--- js/app.js
+++ js/app.js
@@ -2,23 +2,3 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Successfully loaded.");
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const scrollButton = document.querySelector('.scroll-top-btn');
-
-
-    window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 300) {
-            scrollButton.style.display = 'flex';
-        } else {
-            scrollButton.style.display = 'none';
-        }
-    });
-
-
-    scrollButton.addEventListener('click', function () {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
-});
